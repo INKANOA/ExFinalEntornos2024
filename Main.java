1fc95ae034c99e965eb809213221c726afaf31f0
INKANOA <marcos.gilsanz.mojon@estudiante.epsum.school>
2024-06-04 09:48:34 +0200

    Cambios realizados

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
+o
 // Esto debería ser el código de un programa que calcule la hipotenusa dado un tamaño de los dos lados
 // catetos de un triángulo rectángulo que introduzca el usuario.
 
 public class Main {
 	public static void main(String[] args){
-		
+		System.out.println("Cambios")		
 	}
 }
